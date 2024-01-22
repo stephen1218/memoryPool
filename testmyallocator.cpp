80e49124b234571019b10f9905ea558ab2b97b3b
stephen1218 <51876201+stephen1218@users.noreply.github.com>
2024-01-22 21:40:18 +0800

    Add files via upload

---
+++ testmyallocator.cpp
@@ -0,0 +1,23 @@
+//#include "pch.h"
+#include "myallocator.h"
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int, myallocator<int>> vec;
+
+	for (int i = 0; i < 100; ++i)
+	{
+		int data = rand() % 1000;
+		vec.push_back(data);
+	}
+
+	for (int val : vec)
+	{
+		cout << val << " ";
+	}
+	cout<<endl;
+
+	return 0;
+}
